ac36484ecfe4990fa744c356cde62ffa1461ca01
Jagan Singh <jagansiiinghchoudhary@gmail.com>
2023-01-24 00:23:45 +0000

    auth

---
+++ auth-api/src/main/java/com/switchin/auth/MainVerticle.java
@@ -0,0 +1,14 @@
+package com.switchin.auth;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    vertx.createHttpServer()
+        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+        .listen(8080);
+  }
+
+}
